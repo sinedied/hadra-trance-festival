2313530735f4b4dd5b4d7047b6251cef95a2014e
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2016-06-12 22:13:20 +0200

    Initial work on lineup screen

--- sources/main/screens/lineup/lineup.controller.ts
+++ sources/main/screens/lineup/lineup.controller.ts
@@ -2,12 +2,24 @@ import app from 'main.module';
 import {ILogger, LoggerService} from 'helpers/logger/logger';
 
 export class LineupController {
+  lineup = [];
 
   private logger: ILogger;
 
   constructor(logger: LoggerService) {
     this.logger = logger.getLogger('lineup');
     this.logger.log('init');
+
+    for (let i = 0; i < 50; ++i) {
+      this.lineup[i] = {
+        from: new Date(),
+        to: new Date(),
+        type: 'DJ',
+        artist: 'Shotu vs Driss'
+      };
+    }
+
+
   }
 
 }
