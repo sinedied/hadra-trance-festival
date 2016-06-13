76094662c282491565346719e47671da2b6e39c1
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2016-06-13 13:58:12 +0200

    Added demo publishing scripts

---
+++ gulp/demo.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var path = require('path');
+var gulp = require('gulp');
+var del = require('del');
+var conf = require('../gulpfile.config');
+
+var $ = require('gulp-load-plugins')();
+
+gulp.task('copy:demo', function() {
+  var outPath = 'dist';
+  del.sync([outPath]);
+
+  return gulp.src(path.join('demo', '**/*'))
+    .pipe(gulp.dest(outPath));
+});
+
+gulp.task('build:demo', ['build', 'copy:demo'], function() {
+  return gulp.src(path.join(conf.paths.dist, '**/*'))
+    .pipe(gulp.dest(path.join('dist', conf.paths.dist)));
+});
