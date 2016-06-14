ab556a8ef600fef9262e82ebbbf616703a8d6ad5
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2016-06-14 22:48:26 +0200

    Added floor selection

--- sources/main/screens/lineup/lineup.controller.ts
+++ sources/main/screens/lineup/lineup.controller.ts
@@ -1,17 +1,22 @@
 import app from 'main.module';
 import {ILogger, LoggerService} from 'helpers/logger/logger';
+import gettextFunction = angular.gettext.gettextFunction;
 
 export class LineupController {
-  lineup = [];
+  floors = [];
+  selectedFloor = 0;
 
   private logger: ILogger;
 
-  constructor(logger: LoggerService) {
+  constructor(logger: LoggerService,
+              gettext: gettextFunction) {
+
     this.logger = logger.getLogger('lineup');
     this.logger.log('init');
 
+    let lineup = [];
     for (let i = 0; i < 50; ++i) {
-      this.lineup[i] = {
+      lineup[i] = {
         from: new Date(),
         to: new Date(),
         type: 'DJ',
@@ -19,7 +24,16 @@ export class LineupController {
       };
     }
 
+    this.floors = [
+      {name: gettext('Main'), lineup: lineup},
+      {name: gettext('Alternative'), lineup: lineup},
+      {name: gettext('Chillout'), lineup: lineup},
+    ];
 
+  }
+
+  selectFloor(index: number) {
+    this.selectedFloor = index;
   }
 
 }
