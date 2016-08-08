5b654efddc8aca0bb047f4771fa1ac4dd2649281
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2016-08-08 12:14:52 +0200

    Added sticky controls to player

--- sources/main/screens/player/player.controller.ts
+++ sources/main/screens/player/player.controller.ts
@@ -3,9 +3,13 @@ import {ILogger, LoggerService} from 'helpers/logger/logger';
 
 export class PlayerController {
 
+  stickyControls: boolean = false;
+
   private logger: ILogger;
 
-  constructor(private $cordovaInAppBrowser: any,
+  constructor(private $scope: ng.IScope,
+              private $window: ng.IWindowService,
+              private $cordovaInAppBrowser: any,
               logger: LoggerService) {
 
     this.logger = logger.getLogger('player');
@@ -14,6 +18,14 @@ export class PlayerController {
 
   open(url: string) {
     this.$cordovaInAppBrowser.open(url, '_system');
+  }
+
+  onScroll(scrollDelagte: ionic.scroll.IonicScrollDelegate) {
+    let threshold = this.$window.innerHeight / 2 - 44;  // 50vh - $bar-height
+    let scrollTop = scrollDelagte.getScrollPosition().top;
+    this.$scope.$apply(() => {
+      this.stickyControls = scrollTop >= threshold;
+    });
   }
 
 }
