87ca74d2a4f790bacea667b1001b945eb33ea43c
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2016-08-16 12:02:00 +0200

    Updated data

--- scripts/update-data.sh
+++ scripts/update-data.sh
@@ -8,7 +8,7 @@ export DATA_FOLDER=$CWD/data
 export OUT_DIR=$DATA_FOLDER/out
 export NEW_IMAGES_FOLDER=$OUT_DIR/artists
 export FIXED_IMAGES_FOLDER=$DATA_FOLDER/fixes/artists
-export NEW_DATA_FILE=$OUT_DIR/festival.data
+export NEW_DATA_FILE=$DATA_FOLDER/festival.json
 
 export SRC_FOLDER=$CWD/sources
 export TARGET_DATA_FILE=$SRC_FOLDER/static/data.json
@@ -17,9 +17,11 @@ export TARGET_IMAGES_FOLDER=$SRC_FOLDER/images/
 cd $DATA_FOLDER
 rm -rf $OUT_DIR
 
-# curl http://htf.yohann-bianchi.ovh/festivals -o festival.json
+if [ "$1" == "-online" ] || [ ! -e $NEW_DATA_FILE ]; then
+  curl http://htf.yohann-bianchi.ovh/festivals -o festival.json
+fi
 
-./extract-data.js festival.json $TARGET_DATA_FILE
+./extract-data.js $NEW_DATA_FILE $TARGET_DATA_FILE
 
 cp -Rf $NEW_IMAGES_FOLDER $TARGET_IMAGES_FOLDER
 cp -Rf $FIXED_IMAGES_FOLDER $TARGET_IMAGES_FOLDER
